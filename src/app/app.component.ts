c939c3f590ee2497960d26ce8ef29d87dcd56955
tuminzee <t.sheth33@gmail.com>
2021-03-23 12:46:38 +0530

    Fix Tableau Interactive View
    Add ngx-tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,37 @@
-import { Component, AfterViewInit, ViewChild, ElementRef  } from '@angular/core';
+import { Component } from '@angular/core';
 
-declare var tableau: any;
+// declare var tableau: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
 
-    @ViewChild("vizContainer") vizContainer!: ElementRef;
+  url = "https://public.tableau.com/views/Sehat-IntelVisualization/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link"
+  //   @ViewChild("vizContainer") vizContainer!: ElementRef;
 
-  viz: any;
+  // viz: any;
 
   constructor() { }
 
-  ngAfterViewInit(): void {
-    this.initViz();
+  // ngAfterViewInit(): void {
+  //   this.initViz();
 
-  }
+  // }
 
-  initViz() {
-    const containerDiv = this.vizContainer.nativeElement;
+  // initViz() {
+  //   const containerDiv = this.vizContainer.nativeElement;
 
-    let url = "https://public.tableau.com/views/Sehat-IntelVisualization/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link"
-    let options = {
-        hideTabs: true,
-        onFirstInteractive: () => {
-        }
-    }
+  //   let url = "https://public.tableau.com/views/Sehat-IntelVisualization/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link"
+  //   let options = {
+  //       hideTabs: true,
+  //       onFirstInteractive: () => {
+  //       }
+  //   }
 
-    this.viz = new tableau.Viz(containerDiv, url, options)
-  }
+  //   this.viz = new tableau.Viz(containerDiv, url, options)
+  // }
 
 }
