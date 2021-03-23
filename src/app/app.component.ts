e47947c6ea6dea39ccc275a813116eef55506530
Krunal Vasoya <krunalvasoya@gmail.com>
2021-03-23 12:19:04 +0530

    first commit - tableau api testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef  } from '@angular/core';
+
+declare var tableau: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Tableau-JS-API-test';
+export class AppComponent implements AfterViewInit {
+
+    @ViewChild("vizContainer") vizContainer!: ElementRef;
+
+  viz: any;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initViz();
+
+  }
+
+  initViz() {
+    const containerDiv = this.vizContainer.nativeElement;
+
+    let url = "https://public.tableau.com/views/Sehat-IntelVisualization/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link"
+    let options = {
+        hideTabs: true,
+        onFirstInteractive: () => {
+        }
+    }
+
+    this.viz = new tableau.Viz(containerDiv, url, options)
+  }
+
 }
